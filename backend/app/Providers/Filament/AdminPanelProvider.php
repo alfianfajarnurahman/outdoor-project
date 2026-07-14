016f48a810948c4a32ee1e844125647143f47fea
alfianfajarn <alfianfajarn@gmail.com>
2026-07-14 21:12:57 +0700

    Admin 0 : Setup FilamentTheme

--- backend/app/Providers/Filament/AdminPanelProvider.php
+++ backend/app/Providers/Filament/AdminPanelProvider.php
@@ -29,8 +29,10 @@ class AdminPanelProvider extends PanelProvider
             ->path('admin')
             ->login()
             ->colors([
-                'primary' => Color::Amber,
+                'primary' => Color::hex('#1B4D3E'),
             ])
+            ->brandName('Outdoorku Admin')
+            ->viteTheme('resources/css/filament.css')
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
             ->pages([
